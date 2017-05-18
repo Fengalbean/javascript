9407ade6280f3ab0de47c2d06212be85f94587f2
“albean <ifengdb@163.com>
2017-05-18 10:27:09 +0800

    增加数据结构列表、队列、栈和集合

--- js/util.js
+++ js/util.js
@@ -4,13 +4,33 @@
 
 // 检测数据类型
 function type(o) {
-    var s = Object.prototype.toString.call(o);
-    return s.match(/\[object (.*?)\]/)[1].toLowerCase();
+	var s = Object.prototype.toString.call(o);
+	return s.match(/\[object (.*?)\]/)[1].toLowerCase();
 }
 // 打印日志
-function log (str) {
-    if ( type(str) === 'object' ) {
-        return console.log(JSON.stringify(str)+'\n');
-    }
-    return console.log(str);
+function log(str) {
+	if (type(str) === 'object') {
+		return console.log(JSON.stringify(str) + '\n');
+	}
+	return console.log(str);
 }
+/**
+ * 是否是空对象
+ * @param obj
+ * @returns {boolean}
+ */
+function isEmptyObject(obj) {
+	if (Object.keys) {
+		return !(Object.keys(obj).length > 0)
+	}
+	for (var prop in obj) {
+		if (obj.hasOwnProperty(prop)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+String.prototype.reverse = function () {
+	return Array.prototype.reverse.apply(this.split('')).join('');
+};
